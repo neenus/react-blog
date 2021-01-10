e1a433c27a48d675c11e6e15e5404e065feae3c3
Neenus Gabriel <neenus@live.ca>
2021-01-10 16:39:38 +0000

    added basic delete blog functionality using useState hook

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,20 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-// eslint-disable-next-line no-unused-vars
 const [blogs, setBlogs] = useState([
   { title: "My new website", body: "Lorem ipsum ....", author: "Neenus", id: 1 },
   { title: "Wife rules", body: "Lorem ipsum ....", author: "Rina", id: 2 },
   { title: "My birthday party", body: "Lorem ipsum ....", author: "Kiana", id: 3 },
 ]);
 
+const handleDelete = id => {
+  const newBlogs = blogs.filter(blog => blog.id !== id);
+  setBlogs(newBlogs);
+}
+
   return ( 
     <div className="home">
-      <BlogList blogs={ blogs } title="All Blogs" />
+      <BlogList blogs={ blogs } title="All Blogs" handleDelete={ handleDelete } />
     </div>
    );
 }
