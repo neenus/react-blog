d0950f34b242274febc06a5c172ca6d13359e112
Neenus Gabriel <neenus@live.ca>
2021-01-10 16:22:20 +0000

    added blog list component and rendered blogs in home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+// eslint-disable-next-line no-unused-vars
+const [blogs, setBlogs] = useState([
+  { title: "My new website", body: "Lorem ipsum ....", author: "Neenus", id: 1 },
+  { title: "Wife rules", body: "Lorem ipsum ....", author: "Rina", id: 2 },
+  { title: "My birthday party", body: "Lorem ipsum ....", author: "Kiana", id: 3 },
+]);
+
   return ( 
     <div className="home">
-      <h2>Home Page</h2>
+      <BlogList blogs={ blogs } title="All Blogs" />
     </div>
    );
 }
